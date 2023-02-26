322f8275ccc13b631701b5f9bf461442c267ed8c
torokpeter <tpeter002@gmail.com>
2023-02-26 02:18:43 +0100

    made maps show the way it was ment

--- K&H Parking/MapView.swift
+++ K&H Parking/MapView.swift
@@ -52,13 +52,26 @@ struct MapTakenView: View {
                     Image("parkolo2")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: geometry.size.width * 0.9)
+                        .frame(width: geometry.size.width * 0.95)
                         .position(x: geometry.frame(in: .local).midX, y: geometry.frame(in: .local).midY)
                 }
             }
             
             Spacer()
         }
+    }
+}
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+
+        let r = Double((rgbValue & 0xFF0000) >> 16) / 255.0
+        let g = Double((rgbValue & 0x00FF00) >> 8) / 255.0
+        let b = Double(rgbValue & 0x0000FF) / 255.0
+
+        self.init(red: r, green: g, blue: b)
     }
 }
 
